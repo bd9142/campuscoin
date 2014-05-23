c96e83d1fffa8b5be5cf299946d0651486c85832
Burak <burak@burak-VPCEG1BFX.(none)>
2014-05-23 14:49:22 +0300

    delete all files

--- share/qt/make_windows_icon.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# create multiresolution windows icon
-ICON_SRC=../../src/qt/res/icons/campuscoin.png
-ICON_DST=../../src/qt/res/icons/campuscoin.ico
-convert ${ICON_SRC} -resize 16x16 campuscoin-16.png
-convert ${ICON_SRC} -resize 32x32 campuscoin-32.png
-convert ${ICON_SRC} -resize 48x48 campuscoin-48.png
-convert campuscoin-16.png campuscoin-32.png campuscoin-48.png ${ICON_DST}
-
